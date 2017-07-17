6a29d6b4ee7021dbc8d05a0b11128adbd5247147
Gabriel Paiva Rega <gabrielrega@gmail.com>
2017-07-17 17:37:45 -0300

    Update gasto_federal.R
    Adicionei opção para excluir os gastos com refinanciamento de dívidas da conta. O padrão é não excluir estes gastos.

--- gasto_federal.R
+++ gasto_federal.R
@@ -2,7 +2,7 @@
 ## Retorno a tabela do gasto federal por função para um determinado ano
 ## Ano deve ser maior que 2012
 
-gasto_federal <- function(ano=2017)
+gasto_federal <- function(ano=2017, completo = TRUE)
 {
 
   url = paste0("http://www.portaldatransparencia.gov.br/PortalFuncoes.asp?Exercicio=",ano)
@@ -65,6 +65,49 @@ gasto_federal <- function(ano=2017)
   colnames(tab1) = c("Funcao","Valor")
   tab1[,2] = as.numeric(paste(tab1[,2]))
   
-  return (tab1)
+  if (completo) return (tab1)
   
+  url = paste0("http://www.portaltransparencia.gov.br/PortalFuncoes_Detalhe.asp?Exercicio=",ano,"&codFuncao=28")
+  
+  page = readLines(url)
+  grep("Refin",page)
+  
+  pattern = '<td class=\"colunaValor\">([^<]*)</td>'
+  datalines = grep(pattern,page[290:length(page)],value=TRUE)
+  getexpr = function(s,g)substring(s,g,g+attr(g,'match.length')-1)
+  gg = gregexpr(pattern,datalines)
+  matches = mapply(getexpr,datalines,gg)
+  result = gsub(pattern,'\\1',matches)
+  names(result) = NULL
+  
+  v3 = gsub(".","",result, fixed = TRUE)
+  v3 = as.numeric(gsub(",",".", v3, fixed = TRUE))
+  
+  grep("Finalidade", page)
+  pattern2 = '[0-9][0-9][0-9] - ([^<]*)<'
+  datalines = grep(pattern2,page[1:length(page)],value=TRUE)
+  getexpr = function(s,g)substring(s,g,g+attr(g,'match.length')-1)
+  gg = gregexpr(pattern2,datalines)
+  matches = mapply(getexpr,datalines,gg)
+  result = gsub(pattern,'\\1',matches)
+  names(result) = NULL
+  
+  t3 = substr(result,7,nchar(result)-1)
+  
+  tab2 = data.frame(cbind(t3,v3))
+  colnames(tab2) = c("SubFuncao","Valor")
+  tab2[,2] = as.numeric(paste(tab2[,2]))
+  
+  x = grep("Refin", tab2$SubFuncao)
+  y = 0
+  for (i in x) {
+    y=y+tab2[i,2]
+  }
+  y
+  
+  tab3 = tab1
+  x = grep("Encarg", tab3$Funcao)
+  tab3[x,2] = tab3[x,2] - y
+  
+  return (tab3)
 }
