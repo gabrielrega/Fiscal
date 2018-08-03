913ccaf35fed73abc32f6b7afe661a8f84cb6ce9
Gabriel Paiva Rega <gabrielrega@gmail.com>
2018-08-03 11:13:14 -0300

    Update bcb.R
    Redução de código desnecessário.

--- bcb.R
+++ bcb.R
@@ -1,12 +1,10 @@
 library(jsonlite)
 library(tidyverse)
-library(timetk)
 
 bcb <- function(x = 24363){
-  ibc2 <- read_json(paste0("http://api.bcb.gov.br/dados/serie/bcdata.sgs.", x, "/dados?formato=json"),
+  read_json(paste0("http://api.bcb.gov.br/dados/serie/bcdata.sgs.", x, "/dados?formato=json"),
                     simplifyVector = TRUE) %>%
           as.tibble() %>% 
           mutate(valor = as.numeric(valor)) %>% 
           mutate(data = as.Date(data,"%d/%m/%Y")) 
-  return(ibc2)
-}
+  }
