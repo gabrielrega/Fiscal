864a33e34bd8fe8379f1608d56aa840ddd282ade
Gabriel Paiva Rega <gabrielrega@gmail.com>
2019-01-16 09:18:15 -0200

    Update bcb.R
    Adicionei a opção bcb_ts que funciona da mesma forma porém retorna uma time series tradicional e não usa nenhum pacote.

--- bcb.R
+++ bcb.R
@@ -8,3 +8,13 @@ bcb <- function(x = 24363){
           mutate(valor = as.numeric(valor)) %>% 
           mutate(data = as.Date(data,"%d/%m/%Y")) 
   }
+
+bcb_ts <- function(x = 24363){
+   valor <- read.csv2(paste0("http://api.bcb.gov.br/dados/serie/bcdata.sgs.",
+                   x,
+                   "/dados?formato=csv"))
+    valor2 = valor$valor
+    ano = as.numeric(substr(valor$data[1], 7, 10))
+    mes = as.numeric(substr(valor$data[1], 4, 5))
+    return(ts(valor2, start = c(ano,mes), frequency = 12))
+}
